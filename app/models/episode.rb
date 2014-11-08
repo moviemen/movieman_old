af1dc456da9eb744e2ac5cee69927e176b4a39d3
bmalets <b.malets@gmail.com>
2014-11-08 03:16:32 +0200

    notification worker wait for email feature

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,3 +1,12 @@
 class Episode < ActiveRecord::Base
   has_and_belongs_to_many :users
+
+  after_save :email_on_updates
+
+  private
+
+  def email_on_updates
+    NotificationWorker.perform_async self.id
+  end
+
 end
